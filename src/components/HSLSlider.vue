ce1e7b36b3f89e75ae153ff8b36b581b4b07cf61
liuda <luda.min2003@gmail.com>
2023-11-12 23:07:20 +0200

    fix changing saturation

--- src/components/HSLSlider.vue
+++ src/components/HSLSlider.vue
@@ -1,100 +1,128 @@
 <script>
 export default {
-    name: 'HSLSlider',
-    data() {
-        return {
-        hueValue: 0,
-        saturationValue: 0,
-        lightnessValue: 0,
-        lastEmittedSaturationValue: 0,
-        };
+  name: "HSLSlider",
+  data() {
+    return {
+      hueValue: 0,
+      saturationValue: 0,
+      lightnessValue: 0,
+      lastEmittedSaturationValue: 0,
+    };
+  },
+  methods: {
+    handleMouseUp() {
+      if (this.saturationValue !== this.lastEmittedSaturationValue) {
+        console.log("change: saturation is now " + this.saturationValue);
+        if (Number.isInteger(this.saturationValue)) {
+          this.$emit("change", this.saturationValue);
+          this.lastEmittedSaturationValue = this.saturationValue;
+        }
+      }
     },
-    methods: {
-        handleMouseUp() {
-            if (this.saturationValue !== this.lastEmittedSaturationValue) {
-                this.$emit('change', this.saturationValue);
-                this.lastEmittedSaturationValue = this.saturationValue;
-            }
-        },
-    },
-    
+  },
 };
 </script>
 
-
-
 <template>
-    <div class="slider_container">
-        <div class="hue_saturation">
-            <div class="value_container">
-                <p>Hue: </p>
-                <span class="value">{{ hueValue }}</span>
-            </div>
-            <input type="range" min="0" max="360" v-model="hueValue" class="slider" id="hue_slider">
-        </div>
-        <div class="saturation_container">
-            <div class="value_container">
-                <p>Saturation: </p>
-                <span class="value">{{ saturationValue }}</span>
-            </div>
-            <input type="range" min="0" max="100" v-model="saturationValue" class="slider" id="sat_slider" @input="handleMouseUp">
-        </div>
-        <div class="lightness_container">
-            <div class="value_container">
-                <p>Lightness: </p>
-                <span class="value">{{ lightnessValue }}</span>
-            </div>
-            <input type="range" min="0" max="100" v-model="lightnessValue" class="slider" id="light_slider" >
-        </div>
-        
+  <div class="slider_container">
+    <div class="hue_saturation">
+      <div class="value_container">
+        <p>Hue:</p>
+        <span class="value">{{ hueValue }}</span>
+      </div>
+      <input
+        type="range"
+        min="0"
+        max="360"
+        v-model="hueValue"
+        class="slider"
+        id="hue_slider"
+      />
     </div>
+    <div class="saturation_container">
+      <div class="value_container">
+        <p>Saturation:</p>
+        <span class="value">{{ saturationValue }}</span>
+      </div>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        v-model="saturationValue"
+        class="slider"
+        id="sat_slider"
+        @input="handleMouseUp"
+      />
+    </div>
+    <div class="lightness_container">
+      <div class="value_container">
+        <p>Lightness:</p>
+        <span class="value">{{ lightnessValue }}</span>
+      </div>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        v-model="lightnessValue"
+        class="slider"
+        id="light_slider"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .slider_container {
-    display: flex;
-    align-items: left;
-    justify-content: center;
-    flex-direction: column;
-    background-color: #F5F5F5;
-    padding: 32px 16px;
-    border-radius: 16px;
-    width: 300px;
+  display: flex;
+  align-items: left;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #f5f5f5;
+  padding: 32px 16px;
+  border-radius: 16px;
+  width: 300px;
 }
 .value_container {
-    display: flex;
-    flex-direction: row;
-    margin-bottom: 16px;
-    gap: 8px;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 16px;
+  gap: 8px;
 }
 p {
-    margin: 0;
+  margin: 0;
 }
 input {
-    margin: 0;
+  margin: 0;
 }
 .slider {
-    height: 20px;
-    -webkit-appearance: none;
-    border-radius: 10px;
-    width: 100%;
+  height: 20px;
+  -webkit-appearance: none;
+  border-radius: 10px;
+  width: 100%;
 }
 .slider::-webkit-slider-thumb {
-    cursor: pointer;
-    appearance: none;
-    height: 20px;
-    width: 20px;
-    background: none;
-    border: 2px solid #FFFFFF;
-    border-radius: 10px;
+  cursor: pointer;
+  appearance: none;
+  height: 20px;
+  width: 20px;
+  background: none;
+  border: 2px solid #ffffff;
+  border-radius: 10px;
 }
-#hue_slider{
-    background: linear-gradient(to right, #FF0000, #FFF500, #33FF00, #0500FF, #FF0000);
+#hue_slider {
+  background: linear-gradient(
+    to right,
+    #ff0000,
+    #fff500,
+    #33ff00,
+    #0500ff,
+    #ff0000
+  );
 }
 #sat_slider {
-    background: linear-gradient(to right, #767676, #FFF500);
+  background: linear-gradient(to right, #767676, #fff500);
 }
 #light_slider {
-    background: linear-gradient(to right, #000000, #FFF500, #FFFFFF);
+  background: linear-gradient(to right, #000000, #fff500, #ffffff);
 }
 </style>
--- src/views/ColorView.vue
+++ src/views/ColorView.vue
@@ -1,13 +1,13 @@
 <script>
-import ActionButton from '../components/ActionButton.vue'
-import RenderContainer from '../components/RenderSide.vue'
-import Popup from '../components/InfoPopup.vue'
-import ColorButtons from '../components/ColorButton.vue'
-import HSLSlider from '../components/HSLSlider.vue'
-import CMYKSlider from '../components/CMYKSlider.vue'
+import ActionButton from "../components/ActionButton.vue";
+import RenderContainer from "../components/RenderSide.vue";
+import Popup from "../components/InfoPopup.vue";
+import ColorButtons from "../components/ColorButton.vue";
+import HSLSlider from "../components/HSLSlider.vue";
+import CMYKSlider from "../components/CMYKSlider.vue";
 
 export default {
-  name: 'ColorView',
+  name: "ColorView",
   components: {
     ActionButton,
     RenderContainer,
@@ -23,9 +23,10 @@ export default {
       showCMYK: false,
       showPicture: false,
       showUploadMenu: true,
-      popupTitle: 'Color',
-      popupText: 'Color models like HSL (Hue, Saturation, Lightness) and CMYK (Cyan, Magenta, Yellow, Key/Black) play crucial roles in digital design and printing. These models help in achieving precise control over colors in various applications.\nHSL provides a user-friendly approach to manipulating colors. Hue represents the type of color, saturation controls the intensity, and lightness manages the brightness. It`s a popular choice for artists and designers seeking to create visually pleasing and harmonious color schemes. \nIn contrast, CMYK is essential in the printing industry. It combines cyan, magenta, yellow, and black inks to produce a wide spectrum of colors. Printers use this model to ensure accurate color reproduction in magazines, brochures, and other printed materials.\nBy integrating these color models into fractal generation, you can create captivating and customizable fractal art. These models allow for precise adjustments of color gradients, providing a vibrant and dynamic visual experience in fractal patterns. Whether for digital art or printing projects, understanding and utilizing HSL and CMYK in fractal design opens up a world of creative possibilities.',
-    }
+      popupTitle: "Color",
+      popupText:
+        "Color models like HSL (Hue, Saturation, Lightness) and CMYK (Cyan, Magenta, Yellow, Key/Black) play crucial roles in digital design and printing. These models help in achieving precise control over colors in various applications.\nHSL provides a user-friendly approach to manipulating colors. Hue represents the type of color, saturation controls the intensity, and lightness manages the brightness. It`s a popular choice for artists and designers seeking to create visually pleasing and harmonious color schemes. \nIn contrast, CMYK is essential in the printing industry. It combines cyan, magenta, yellow, and black inks to produce a wide spectrum of colors. Printers use this model to ensure accurate color reproduction in magazines, brochures, and other printed materials.\nBy integrating these color models into fractal generation, you can create captivating and customizable fractal art. These models allow for precise adjustments of color gradients, providing a vibrant and dynamic visual experience in fractal patterns. Whether for digital art or printing projects, understanding and utilizing HSL and CMYK in fractal design opens up a world of creative possibilities.",
+    };
   },
   methods: {
     closePopup() {
@@ -40,11 +41,11 @@ export default {
       this.showCMYK = true;
     },
     exportData() {
-      const canvas = document.querySelector('canvas');
-      const imageDataURL = canvas.toDataURL('image/jpg');
-      const downloadLink = document.createElement('a');
+      const canvas = document.querySelector("canvas");
+      const imageDataURL = canvas.toDataURL("image/jpg");
+      const downloadLink = document.createElement("a");
       downloadLink.href = imageDataURL;
-      downloadLink.download = 'modified_image.jpg';
+      downloadLink.download = "modified_image.jpg";
       downloadLink.click();
     },
     handleImageUpload(event) {
@@ -59,7 +60,7 @@ export default {
 
           image.onload = () => {
             const canvas = this.$refs.canvas;
-            const ctx = canvas.getContext('2d');
+            const ctx = canvas.getContext("2d");
 
             const canvasWidth = 900;
             const canvasHeight = 500;
@@ -87,78 +88,91 @@ export default {
       }
     },
     closePicture() {
-        this.showPicture = false;
-        this.showUploadMenu = true;
+      this.showPicture = false;
+      this.showUploadMenu = true;
     },
     handleHSLChange(saturationValue) {
-        this.updateYellowSaturation(saturationValue);
+      this.updateYellowSaturation(saturationValue);
     },
-    updateYellowSaturation(value) {
-        console.log("saturation we get: " + value);
-        const canvas = this.$refs.canvas;
-        const ctx = canvas.getContext('2d');
+    updateYellowSaturation(event) {
+      const value = parseInt(event.target.value);
+      console.log("saturation we get: ");
+      console.log(value);
+      const canvas = this.$refs.canvas;
+      const ctx = canvas.getContext("2d");
 
-        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        const data = imageData.data;
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-        // range of yellow color in HSL
-        const yellowHueMin = 40;
-        const yellowHueMax = 80;
-        const yellowSaturationMin = 50;
-        const yellowSaturationMax = 100;
+      // range of yellow color in HSL
+      const yellowHueMin = 40;
+      const yellowHueMax = 80;
+      const yellowSaturationMin = 50;
+      const yellowSaturationMax = 100;
 
-        for (let i = 0; i < data.length; i += 4) {
-            const red = data[i];
-            const green = data[i + 1];
-            const blue = data[i + 2];
+      for (let i = 0; i < imageData.data.length; i += 4) {
+        const red = imageData.data[i];
+        const green = imageData.data[i + 1];
+        const blue = imageData.data[i + 2];
 
-            const hsl = RGBToHSL(red, green, blue);
-
-            // check if color is yellow
-            if (hsl[0] >= yellowHueMin && hsl[0] <= yellowHueMax && hsl[1] >= yellowSaturationMin && hsl[1] <= yellowSaturationMax) 
-            {
-                hsl[1] = value;
-                console.log(hsl[1]);
-                const rgb = HSLToRGB(hsl[0], hsl[1], hsl[2]);
-                data[i] = rgb[0];
-                data[i + 1] = rgb[1];
-                data[i + 2] = rgb[2];
-            }
+        const hsl = RGBToHSL(red, green, blue);
+        // check if color is yellow
+        const isYellowColor =
+          hsl[0] >= yellowHueMin &&
+          hsl[0] <= yellowHueMax &&
+          hsl[1] >= yellowSaturationMin &&
+          hsl[1] <= yellowSaturationMax;
+        if (isYellowColor) {
+          hsl[1] = value;
+          const rgb = HSLToRGB(hsl[0], hsl[1], hsl[2]);
+          imageData.data[i] = rgb[0];
+          imageData.data[i + 1] = rgb[1];
+          imageData.data[i + 2] = rgb[2];
         }
+      }
 
-        ctx.putImageData(imageData, 0, 0);
+      ctx.putImageData(imageData, 0, 0);
     },
     handleCMYKChange(cyan, magenta, yellow, black) {
-        const canvas = this.$refs.canvas;
-        const ctx = canvas.getContext('2d');
+      const canvas = this.$refs.canvas;
+      const ctx = canvas.getContext("2d");
 
-            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            const data = imageData.data;
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const data = imageData.data;
 
-            const rgb = CMYKToRGB(cyan / 100, magenta / 100, yellow / 100, black / 100);
+      const rgb = CMYKToRGB(
+        cyan / 100,
+        magenta / 100,
+        yellow / 100,
+        black / 100
+      );
 
-            const yellowMin = [240, 190, 50];  
-            const yellowMax = [255, 255, 100];
+      const yellowMin = [240, 190, 50];
+      const yellowMax = [255, 255, 100];
 
-            for (let i = 0; i < data.length; i += 4) {
-                const red = data[i];
-                const green = data[i + 1];
-                const blue = data[i + 2];
+      for (let i = 0; i < data.length; i += 4) {
+        const red = data[i];
+        const green = data[i + 1];
+        const blue = data[i + 2];
 
-                // check if color is yellow in RGB
-                if (red >= yellowMin[0] && red <= yellowMax[0] && green >= yellowMin[1] && green <= yellowMax[1] && blue >= yellowMin[2] && blue <= yellowMax[2]) 
-                {
-                    data[i] = rgb[0];
-                    data[i + 1] = rgb[1];
-                    data[i + 2] = rgb[2];
-                }
-            }
+        // check if color is yellow in RGB
+        if (
+          red >= yellowMin[0] &&
+          red <= yellowMax[0] &&
+          green >= yellowMin[1] &&
+          green <= yellowMax[1] &&
+          blue >= yellowMin[2] &&
+          blue <= yellowMax[2]
+        ) {
+          data[i] = rgb[0];
+          data[i + 1] = rgb[1];
+          data[i + 2] = rgb[2];
+        }
+      }
 
-            ctx.putImageData(imageData, 0, 0);
-    }
-
-  }
-}
+      ctx.putImageData(imageData, 0, 0);
+    },
+  },
+};
 function RGBToHSL(r, g, b) {
   // Make r, g, and b fractions of 1
   r /= 255;
@@ -199,31 +213,42 @@ function RGBToHSL(r, g, b) {
   return [h, s, l];
 }
 
-
 function HSLToRGB(h, s, l) {
   // Must be fractions of 1
   s /= 100;
   l /= 100;
 
   let c = (1 - Math.abs(2 * l - 1)) * s,
-      x = c * (1 - Math.abs((h / 60) % 2 - 1)),
-      m = l - c / 2,
-      r = 0,
-      g = 0,
-      b = 0;
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+    m = l - c / 2,
+    r = 0,
+    g = 0,
+    b = 0;
 
   if (0 <= h && h < 60) {
-    r = (c + m) * 255; g = (x + m) * 255; b = m * 255;
+    r = (c + m) * 255;
+    g = (x + m) * 255;
+    b = m * 255;
   } else if (60 <= h && h < 120) {
-    r = (x + m) * 255; g = (c + m) * 255; b = m * 255;
+    r = (x + m) * 255;
+    g = (c + m) * 255;
+    b = m * 255;
   } else if (120 <= h && h < 180) {
-    r = m * 255; g = (c + m) * 255; b = (x + m) * 255;
+    r = m * 255;
+    g = (c + m) * 255;
+    b = (x + m) * 255;
   } else if (180 <= h && h < 240) {
-    r = m * 255; g = (x + m) * 255; b = (c + m) * 255;
+    r = m * 255;
+    g = (x + m) * 255;
+    b = (c + m) * 255;
   } else if (240 <= h && h < 300) {
-    r = (x + m) * 255; g = m * 255; b = (c + m) * 255;
+    r = (x + m) * 255;
+    g = m * 255;
+    b = (c + m) * 255;
   } else if (300 <= h && h < 360) {
-    r = (c + m) * 255; g = m * 255; b = (x + m) * 255;
+    r = (c + m) * 255;
+    g = m * 255;
+    b = (x + m) * 255;
   }
 
   r = Math.round(r);
@@ -233,119 +258,130 @@ function HSLToRGB(h, s, l) {
   return [r, g, b];
 }
 function CMYKToRGB(c, m, y, k) {
-    const r = 255 * (1 - c) * (1 - k);
-    const g = 255 * (1 - m) * (1 - k);
-    const b = 255 * (1 - y) * (1 - k);
+  const r = 255 * (1 - c) * (1 - k);
+  const g = 255 * (1 - m) * (1 - k);
+  const b = 255 * (1 - y) * (1 - k);
 
-    return [Math.round(r), Math.round(g), Math.round(b)];
+  return [Math.round(r), Math.round(g), Math.round(b)];
 }
 </script>
 
 <template>
-    <div class="container">
-      <div class="content_container">
-        <div class="buttons">
-          <div class="header_buttons">
-            <ColorButtons @hsl="showHSLComponent" @cmyk="showCMYKComponent"></ColorButtons>
-          </div>
-          <HSLSlider v-if="showHSL" @change="handleHSLChange"/>
-          <CMYKSlider v-if="showCMYK" @change-cmyk="handleCMYKChange"/>
-          <div class="footer_buttons">
-            <ActionButton @click="showPopup = true">Learn more</ActionButton>
-            <ActionButton @click="exportData">Save as</ActionButton>
-          </div>
+  <div class="container">
+    <div class="content_container">
+      <div class="buttons">
+        <div class="header_buttons">
+          <ColorButtons
+            @hsl="showHSLComponent"
+            @cmyk="showCMYKComponent"
+          ></ColorButtons>
         </div>
-        <RenderContainer>
-          <div v-if="showPicture" class="uploaded_image_container">
-            <canvas ref="canvas" width="900" height="500"></canvas>
-            <img class="close_picture" src="../assets/cancel.png" @click="closePicture">
-          </div>
-          <div class="upload_image_container" v-if="showUploadMenu">
-            <p>Upload an image</p>
-            <label class="custom-file-upload">
-              <input type="file" accept="image/*" @change="handleImageUpload">
-              Select File
-            </label>
-          </div>
-        </RenderContainer>
+        <HSLSlider v-if="showHSL" @change="handleHSLChange" />
+        <CMYKSlider v-if="showCMYK" @change-cmyk="handleCMYKChange" />
+        <div class="footer_buttons">
+          <ActionButton @click="showPopup = true">Learn more</ActionButton>
+          <ActionButton @click="exportData">Save as</ActionButton>
+        </div>
       </div>
+      <RenderContainer>
+        <div v-if="showPicture" class="uploaded_image_container">
+          <canvas ref="canvas" width="900" height="500"></canvas>
+          <img
+            class="close_picture"
+            src="../assets/cancel.png"
+            @click="closePicture"
+          />
+        </div>
+        <div class="upload_image_container" v-if="showUploadMenu">
+          <p>Upload an image</p>
+          <label class="custom-file-upload">
+            <input type="file" accept="image/*" @change="handleImageUpload" />
+            Select File
+          </label>
+        </div>
+      </RenderContainer>
     </div>
-    <Popup v-if="showPopup" :title="popupTitle" :text="popupText" @close="closePopup"></Popup>
-  </template>
-  
-  <style scoped>
-  .container {
-    margin-top: 66.4px;
-    padding: 32px 48px;
-    height: 643px;
-  }
-  .content_container {
-    display: grid;
-    gap: 3%;
-    grid-template-columns: 30% 67%;
-    height: 100%;
-  }
-  .buttons {
-    display: flex;
-    height: 100%;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .header_buttons {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-  .upload_image_container {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 16px;
-    cursor: pointer;
-  }
-  p {
-    margin: 0;
-  }
-  .footer_buttons {
-    display: flex;
-    width: 100%;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-  .custom-file-upload {
-    display: inline-block;
-    padding: 6px 12px;
-    cursor: pointer;
-    color: #fff;
-    background-color: #7EC58E;
-    border: none;
-    border-radius: 24px;
-  }
-  .custom-file-upload input {
-    display: none;
-  }
-  .uploaded_image_container {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    gap: 16px;
-    margin: 30px;
-    position: relative;
-  }
-  
-  .uploaded_image_container img {
-    max-width: 100%;
-    max-height: 100%;
-    object-fit: cover;
-  }
-  
-  .close_picture {
-    position: absolute; 
-    top: 0;
-    right: 0;
-    cursor: pointer;
-  }
-  </style>
-  
+  </div>
+  <Popup
+    v-if="showPopup"
+    :title="popupTitle"
+    :text="popupText"
+    @close="closePopup"
+  ></Popup>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 66.4px;
+  padding: 32px 48px;
+  height: 643px;
+}
+.content_container {
+  display: grid;
+  gap: 3%;
+  grid-template-columns: 30% 67%;
+  height: 100%;
+}
+.buttons {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+.header_buttons {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.upload_image_container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 16px;
+  cursor: pointer;
+}
+p {
+  margin: 0;
+}
+.footer_buttons {
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.custom-file-upload {
+  display: inline-block;
+  padding: 6px 12px;
+  cursor: pointer;
+  color: #fff;
+  background-color: #7ec58e;
+  border: none;
+  border-radius: 24px;
+}
+.custom-file-upload input {
+  display: none;
+}
+.uploaded_image_container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 16px;
+  margin: 30px;
+  position: relative;
+}
+
+.uploaded_image_container img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: cover;
+}
+
+.close_picture {
+  position: absolute;
+  top: 0;
+  right: 0;
+  cursor: pointer;
+}
+</style>
